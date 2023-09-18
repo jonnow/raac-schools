00d8dc1e08cce419be57cbbf17be02025a73807f
jonnow <jonnowitts@gmail.com>
2023-09-18 09:09:55 +0100

    Update package with SQLite, add basic analytics data

--- index.js
+++ index.js
@@ -3,142 +3,62 @@ const fastify = require('fastify')({
   logger: true
 })
 const join = require('path').join
+const db = require("./sqlite.js");
+const errorMessage =
+  "Whoops! Error connecting to the database–please try again!";
 
 const { GetSchoolCoords } = require('./controllers/schoolData');
 
 fastify.register(require("@fastify/view"), {
-    engine: {
-        handlebars: require("handlebars"),
-    },
-    root: join(__dirname, "views"),
-    layout: "./templates/layout",
-    templates: join(__dirname, './views/templates/'),
-    viewExt: 'hbs',
-    options: {
-        partials: {
-            head: '/partials/head.hbs'
-        }
-    },
+  engine: {
+    handlebars: require("handlebars"),
+  },
+  root: join(__dirname, "views"),
+  layout: "./templates/layout",
+  templates: join(__dirname, './views/templates/'),
+  viewExt: 'hbs',
+  options: {
+    partials: {
+      head: '/partials/head.hbs'
+    }
+  },
 });
 
 fastify.register(require("@fastify/static"), {
-    root: join(__dirname, 'public'),
-    prefix: '/public/',
+  root: join(__dirname, 'public'),
+  prefix: '/public/',
 })
 
-fastify.decorate('notFound', (request, reply) => {
-  reply.code(404).view('404', {pageTitle: '404 - Page not found'})
+fastify.decorate('notFound', (req, reply) => {
+  reply.code(404).view('404', { pageTitle: '404 - Page not found' })
 })
 
 fastify.setNotFoundHandler(fastify.notFound)
 
-
-
-fastify.get('/', async (request, reply) => {
-    
-    return reply.view('index', {pageTitle: 'RAAC Schools map'})
+fastify.addHook('preHandler', (req, reply, done) => {
+  done()
 })
 
-fastify.get('/getSchools', async(request, reply) => {
-    const schoolsData = await GetSchoolCoords()
-    console.log('data: ', schoolsData)
-    return reply.send({schools: schoolsData})
+fastify.get('/', async (req, reply) => {
+  logVisit(req)
+  return reply.view('index', { pageTitle: 'RAAC Schools map' })
 })
 
-fastify.get('/about', (request, reply) => {
-    return reply.view('about', {pageTitle: 'About the RAAC Schools map'})
+fastify.get('/getSchools', async (req, reply) => {
+  logVisit(req)
+  const schoolsData = await GetSchoolCoords()
+  return reply.send({ schools: schoolsData })
 })
 
-fastify.get('/data', (request, reply) => {
-  return reply.view('data', {pageTitle: 'Data used by this website'})
+fastify.get('/about', (req, reply) => {
+  logVisit(req)
+  return reply.view('about', { pageTitle: 'About the RAAC Schools map' })
 })
 
-// fastify.get('/update', (request, reply) => {
-//     if(request.query.postcodes == 1) {
-//         console.log('Updating postcodes...')
-//     }
-
-//     if(request.query.convertCSV == 1) {
-//         let fileInputName = './public/data/edubasealldata20230910.csv'; 
-//         let fileOutputName = './public/data/edubasealldata20230910.json';
-//         let raacSchools = require('./public/data/raac_data.json');
-//         let results = [], allSchools = [];
-
-//         // csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
-//         fs.createReadStream(fileInputName).pipe(csv({}))
-//         .on('data', (data) => allSchools.push(data))
-//         .on('end', () => {
-//             allSchools.map((school) => {
-//                 const foundSchool = raacSchools.find((raacSchool) => raacSchool.URN == school.URN)
-//                 if(foundSchool) {
-//                     const newData = {
-//                         ...foundSchool,
-//                         ...school
-//                     }
-//                     results.push(newData)
-//                 }
-//             })
-
-//             let strResults = JSON.stringify(results);
-            
-//             fs.writeFile(fileOutputName, strResults, (err) => {
-//                 let msg, status;
-//                 if(err) {
-//                     msg = `Error writing file, no data written. Here is a readout of the data that did not write: \n${newData}`
-//                     status = 'failure'
-//                     console.error(`${status}: ${msg}`)
-//                 }
-//                 else {
-//                     msg = `Writing new data file complete.\nData successfully written to new file ${fileOutputName}.`
-//                     status = 'success'
-//                     console.info(`${status}: ${msg}`)
-//                 }
-//                 return reply.send({
-//                     status: msg
-//                 })
-//             })
-//         })
-//     }
-
-//     if(request.query.addLonLat == 1) {
-//         const fileInputName = './public/data/full_raac_data.json'
-//         const fileOutputName = './public/data/fuller_raac_data.json'
-
-//         const dataFile = fs.readFile(fileInputName, (err, data) => {
-//             const lonLatDataFile = fs.readFileSync('./private/lonlatdata.json')
-//             const lonLatData = JSON.parse(lonLatDataFile)
-//             const jsonData = JSON.parse(data)
-//             jsonData.map(school => {
-//                 const result = lonLatData.find(el=>el.UPRN == school.UPRN)
-//                 try {
-//                     school.coordinates = [result.Latitude, result.Longitude]
-                    
-//                 } catch (error) {
-//                     debugger
-//                 }
-//             })
-            
-//             const strData = JSON.stringify(jsonData)
-//             fs.writeFile(fileOutputName, strData, (err) => {
-//                 let msg, status;
-//                 if(err) {
-//                     msg = `Error writing file, no data written. Here is a readout of the data that did not write: \n${newData}`
-//                     status = 'failure'
-//                     console.error(`${status}: ${msg}`)
-//                 }
-//                 else {
-//                     msg = `Writing new data file complete.\nData successfully written to new file ${fileOutputName}.`
-//                     status = 'success'
-//                     console.info(`${status}: ${msg}`)
-//                 }
-//                 return reply.send({
-//                     status: msg
-//                 })
-//             })
-//         })
-//     }
-// })
-//https://api.ideal-postcodes.co.uk/v1/addresses?api_key=iddqd&lon=-0.12767&lat=51.503541
+fastify.get('/data', (req, reply) => {
+  logVisit(req)
+  return reply.view('data', { pageTitle: 'Data used by this website' })
+})
 
 /**
  * Run the server!
@@ -152,3 +72,14 @@ const start = async () => {
   }
 }
 start()
+
+const logVisit = (req) => {
+  const userHeaders = req.headers
+  const user = {
+    method: req.method,
+    url: req.url,
+    lang: userHeaders['accept-language'],
+    userAgent: userHeaders['user-agent']
+  }
+  db.addVisit(user);
+}
---
+++ sqlite.js
@@ -0,0 +1,47 @@
+/**
+ * Module handles database management
+ *
+ * The sample data is for a chat log with one table:
+ * Messages: id + message text
+ */
+
+const fs = require("fs");
+const dbFile = "./.data/analytics.db";
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require("sqlite3").verbose();
+const dbWrapper = require("sqlite");
+let db;
+
+//SQLite wrapper for async / await connections https://www.npmjs.com/package/sqlite
+dbWrapper
+    .open({
+        filename: dbFile,
+        driver: sqlite3.Database
+    })
+    .then(async dBase => {
+        db = dBase;
+
+        try {
+            if (!exists) {
+                await db.run(
+                    "CREATE TABLE Analytics (id INTEGER PRIMARY KEY AUTOINCREMENT, method TEXT, url TEXT, lang TEXT, userAgent TEXT)"
+                );
+            }
+            //console.log(await db.all("SELECT * from Analytics"));
+        } catch (dbError) {
+            console.error(dbError);
+        }
+    });
+
+module.exports = {
+    // Add new message
+    addVisit: async user => {
+        let success = false;
+        try {
+            success = await db.run(`INSERT INTO Analytics (method, url, lang, userAgent) VALUES ('${user.method}', '${user.url}', '${user.lang}', '${user.userAgent}')`);
+        } catch (dbError) {
+            console.error(dbError);
+        }
+        return success.changes > 0 ? true : false;
+    },
+}
